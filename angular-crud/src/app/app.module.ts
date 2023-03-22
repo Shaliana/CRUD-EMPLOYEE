aa8be96ec614b9c9c338f18ead883e38d48eaedb
shaliana <shaliana9putri@gmail.com>
2023-03-23 00:34:40 +0700

    create routing to api crud employee

---
+++ angular-crud/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+import { EmployeeDetailsComponent } from './components/employee-details/employee-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddEmployeeComponent,
+    EmployeeListComponent,
+    EmployeeDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
